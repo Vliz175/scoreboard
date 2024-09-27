cc85159d97242581de91a08d927b99d326d05451
fauzan zaki <fauzanz4k1@gmail.com>
2024-09-27 20:35:17 +0700

    fixing behaviour in javascript

--- js/main.js
+++ js/main.js
@@ -5,24 +5,34 @@ const btn2 = document.querySelector(".p2");
 const resetBtn = document.querySelector("#reset");
 
 function reset() {
-  isGmaeOver = false;
+  isGameOver = false;
   score1 = 0;
   score2 = 0;
   displayScore[0].textContent = score1;
   displayScore[1].textContent = score2;
+  btn1.disabled = false;
+  btn2.disabled = false;
 }
 
 let score1 = 0;
 let score2 = 0;
 let maxScore = 0;
-let isGmaeOver = false;
+let isGameOver = true;
+
+btn1.disabled = true;
+btn2.disabled = true;
 
 btn1.addEventListener("click", function () {
   score1 += 1;
   displayScore[0].textContent = score1;
   if (score1 == maxScore || score2 == maxScore) {
     console.log(true);
-    isGmaeOver = true;
+    isGameOver = true;
+  }
+
+  if (isGameOver == true) {
+    btn1.disabled = true;
+    btn2.disabled = true;
   }
 });
 
@@ -31,13 +41,22 @@ btn2.addEventListener("click", function () {
   displayScore[1].textContent = score2;
   if (score1 == maxScore || score2 == maxScore) {
     console.log(true);
-    isGmaeOver = true;
+    isGameOver = true;
+  }
+
+  if (isGameOver == true) {
+    btn1.disabled = true;
+    btn2.disabled = true;
   }
 });
 
 scoreOption.addEventListener("change", function () {
   maxScore = this.value;
   reset();
+  if (score1 == maxScore || score2 == maxScore) {
+    btn1.disabled = true;
+    btn2.disabled = true;
+  }
 });
 
 resetBtn.addEventListener("click", reset);
